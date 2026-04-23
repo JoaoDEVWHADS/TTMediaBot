a035a0e224837bb29e32ffc4e92423cf223918a9
jjrg311 <jjrg311@users.noreply.github.com>
2026-04-23 06:32:18 +0000

    Test: Final verification of version-tracked auto-rebuild

--- update.sh
+++ update.sh
@@ -329,3 +329,4 @@ install_deps_light
 update_and_fix_permissions
 configure_auto_updater
 # Automated update test - Thu Apr 23 06:11:32 UTC 2026
+# Version tracking test - Thu Apr 23 06:32:18 UTC 2026
